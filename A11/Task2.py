2ad1a369bbff896ea2909d9b356898244b36e3d8
aamirriaz2010-sudo <aamirriaz2010@gmail.com>
2026-09-18 10:50:36 +1000

    Task2 and 5

--- A11/Task2.py
+++ A11/Task2.py
@@ -1,7 +1,9 @@
 n = int(input("Number to test is: "))
-for i in range (0,n):
-    x = []
-    if n % x == 0 and (x1 + x2 + xn) == n:
-        print("n is a perfect number")
-    else:
-        print("n is not a perfect number")
+divisibility = []
+for i in range (1,n):
+    if n % i == 0:
+        divisibility.append(i)
+if sum(divisibility) == n:
+    print(f"{n} is a perfect number")
+else:
+    print(f"{n} is not a perfect number")
--- A11/Task5.py
+++ A11/Task5.py
@@ -1,5 +1,15 @@
 Results = dict()
-Results['Alice'] = '85'
-Results['Bob'] = '72'
-Results['Charlie'] = '96'
+Marks = [85 , 75 , 32]
+Students = ["Alice" , "Bob" , "Charlie"]
+Results["Alice"]= Marks[0]
+Results["Bob"] = Marks[1]
+Results["Charlie"] = Marks[2]
+
+def Level():
+    Results = dict()
+    Marks = [85 , 75 , 32]
+    Students = ["Alice" , "Bob" , "Charlie"]
+    Students[0] = Marks[0]
+    Students[1] = Marks[1]
+    Students [2] = Marks[2]
 print(Results)
